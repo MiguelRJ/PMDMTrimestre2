8c6874e1775aa915237896ac9d7efce2af49fae8
MiguelRJ <miguel.rj96@gmail.com>
2017-12-15 10:14:44 +0100

    clase ciclo de enventos

---
+++ Clase1215ScriptCiclo/Assets/ciclo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ciclo : MonoBehaviour {
+
+	void Awake (){
+		Debug.Log ("Awake...");
+	}
+		
+	void Start () {
+		Debug.Log ("Start...");
+	}
+
+	void FixedUpdate (){
+		Debug.Log ("FixedUpdate...");
+	}
+
+	void Update () {
+		Debug.Log ("Update...");
+	}
+
+	void LateUpdate (){
+		Debug.Log ("LateUpdate...");
+	}
+
+	void OnGUI (){
+		Debug.Log ("OnGui...");
+	}
+}
