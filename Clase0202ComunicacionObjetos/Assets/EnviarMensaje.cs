38c326db3ff9dff1842048cb3b09ef04f7f99a9e
MiguelRJ <miguel.rj96@gmail.com>
2018-02-02 10:06:28 +0100

    sendmessage

---
+++ Clase0202ComunicacionObjetos/Assets/EnviarMensaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Asignado a emisor
+public class EnviarMensaje : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		// Lanzar mensaje
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject.FindGameObjectWithTag ("Receptor").SendMessage("MostrarDatos","soy un message");
+		}
+	}
+}
---
+++ Clase0202ComunicacionObjetos/Assets/RecibirMensaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Asignado a receptor
+public class RecibirMensaje : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void MostrarDatos(string value){
+		Debug.Log ("Soy el metodo MostrarDatos del receptor " + value);
+	}
+
+}
